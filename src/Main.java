8986f34ac0fe3bf491aa48475b888f65f8ebcf3b
Danilo Santos <danilondosantos@gmail.com>
2024-07-04 13:52:14 -0300

    feat: :sparkles: assigning values to the film

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+        boolean incluidoNoPlano = false;
+        System.out.println("Incluido no plano: " + (incluidoNoPlano ? "Sim" : "Não"));
+
+        double notaDoFilme = 8.1;
+        System.out.println("Nota do filme: " + notaDoFilme);
     }
 }
